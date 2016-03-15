e1d935791a76b34640a78b1984eb0b4f85c727a3
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-15 10:36:39 -0700

    test the lit type

---
+++ lit_test.go
@@ -0,0 +1,33 @@
+package dpll
+
+import "testing"
+
+func TestLit(t *testing.T) {
+	tests := []struct {
+		v     Var
+		pos   bool
+		undef bool
+		s     string
+	}{
+		{Var(1), true, false, "1"},
+		{Var(1), false, false, "-1"},
+		{Var(0), true, true, "0"},
+		{Var(0), false, true, "0"},
+	}
+
+	for i, test := range tests {
+		lit := NewLit(test.v, test.pos)
+		if lit.Var() != test.v {
+			t.Errorf("test %d: variable %d (!= %d)", i, lit.Var(), test.v)
+		}
+		if lit.IsPos() != test.pos {
+			t.Errorf("test %d: positive %v (!= %v)", i, lit.IsPos(), test.pos)
+		}
+		if lit.IsUndef() != test.undef {
+			t.Errorf("test %d: undef %v (!= %v)", i, lit.IsUndef(), test.undef)
+		}
+		if lit.String() != test.s {
+			t.Errorf("test %d: %q (!= %q)", i, lit.String(), test.s)
+		}
+	}
+}
