d269754a7a5dae5d1c180042e25708bf5193bd74
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-03-17 16:51:02 -0700

    add (artifitial) unsat instance to solver tests
    A SAT instance was hacked so that the satisfying assignment was no
    longer possible (e.g. add clauses disallowing at least one of the
    otherwise satisfying assignments).

--- dpll_test.go
+++ dpll_test.go
@@ -19,6 +19,18 @@ func TestSolver_Solve_sat_factoring_3_5(t *testing.T) {
 	}
 }
 
+func TestSolver_Solve_unsat_factoring_3_5(t *testing.T) {
+	d := New(nil)
+	err := DecodeFile(d, "testdata/factoring_3_5_UNSAT.cnf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	sat := d.Solve()
+	if !sat {
+		t.Errorf("not satisfiable")
+	}
+}
+
 func TestSolver_Solve_sat_factoring_5_7(t *testing.T) {
 	d := New(nil)
 	err := DecodeFile(d, "testdata/factoring_5_7.cnf")
